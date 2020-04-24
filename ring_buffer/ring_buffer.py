b8df81280d6b89924da857e6bd7d6abc4087c499
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-24 14:13:03 -0400

    ring_buffer: complete RingBuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,17 +5,44 @@ class RingBuffer:
 
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.current = 0
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+
+        index = self.current % self.capacity
+
+        if index < self.capacity:
+
+            node = self.storage.head
+
+            # move the the node at `index`
+            for __ in range(index):
+                node = node.next
+
+            # if the node exists, replace its value
+            if node:
+                node.value = item
+
+            # else, add a new node
+            else:
+                self.storage.add_to_tail(item)
+
+            self.current += 1
+
+        else:
+            # can't do anything
+            pass
 
     def get(self):
+
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        node = self.storage.head
+        while node is not None:
+            list_buffer_contents.append(node.value)
+            node = node.next
 
         return list_buffer_contents
 
