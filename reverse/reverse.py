62e86d44a6973d55c37231d4061fcef62e2c74fb
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-24 12:02:49 -0400

    reverse.py: adjust whitespace

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,41 +1,59 @@
 class Node:
 
     def __init__(self, value=None, next_node=None):
+
         # the value at this linked list node
         self.value = value
         # reference to the next node in the list
         self.next_node = next_node
 
+        return
+
     def get_value(self):
+
         return self.value
 
     def get_next(self):
+
         return self.next_node
 
     def set_next(self, new_next):
+
         # set this node's next_node reference to the passed in node
         self.next_node = new_next
+
+        return
 
 
 class LinkedList:
 
     def __init__(self):
+
         # reference to the head of the list
         self.head = None
 
+        return
+
     def add_to_head(self, value):
+
         node = Node(value)
+
         if self.head is not None:
             node.set_next(self.head)
 
         self.head = node
 
+        return
+
     def contains(self, value):
+
         if not self.head:
             return False
+
         # get a reference to the node we're currently at; update this as we
         # traverse the list
         current = self.head
+
         # check to see if we're at a valid node
         while current:
             # return True if the current value we're looking at matches our
@@ -44,6 +62,7 @@ class LinkedList:
                 return True
             # update our current node to the current node's next node
             current = current.get_next()
+
         # if we've gotten here, then the target node isn't in our list
         return False
 
